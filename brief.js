6f5eca973ef76aeb44434c8a253bfb537f7d7b97
moreme-m <mariamlaw2011@gmail.com>
2024-12-29 17:59:11 +0100

    classes

--- brief.js
+++ brief.js
@@ -10,29 +10,29 @@ var fruits = ['cherry','watermelon']
 
 
 
-console.log(typeof(x))
-console.log(typeof(y))
-console.log(typeof(a))
-console.log(typeof(b))
-console.log(typeof(c))
-console.log(typeof(student))
-console.log(typeof(fruits))
+document.write(typeof(x))
+document.write(typeof(y))
+document.write(typeof(a))
+document.write(typeof(b))
+document.write(typeof(c))
+document.write(typeof(student))
+document.write(typeof(fruits))
 
 var age = 10
 
 if (age >= 18){
-    console.log("You are old enough")
+    document.write("You are old enough")
 }
 
 else{
-    console.log("You are young")
+    document.write("You are young")
 }
 
 
-console.log("<br>")
+
 //white loop
 let i = 1 //initialize the counter available
 while(i <=10){  //the condition to run the loop
-    console.log(i + "<br>")  //print the current number
+    document.write(i)  //print the current number
     i++; //increment the counter available
 }
---
+++ classes.js
@@ -0,0 +1,24 @@
+// classes in javascript ==> a blueprint/template for creating objects
+// object => a variable that contains key value pairs
+
+let student1 = {
+    name: "Mariam",
+    age: 13,
+    hobbies: ["parachuting", "coding"]
+}
+
+class student {
+    constructor(name, age, hobby, color){
+        this.name = name
+        this.age = age
+         this.hobby = hobby
+         this.color = color
+    }
+}
+
+let student2 = new student("Victor", 19, "dancing", "red")
+let student3 = new student("Devi", 16, "eating", "orange")
+
+document.write("My name is ", student2.name, " and I am ", student2.age, " years old. I love ",student2.hobby, ". My favourite color is ", student2.color)
+document.write("<br>")
+document.write("My name is ", student3.name, " and I am ", student3.age, " years old. I love ", student3.hobby, ". My favourite color is ", student3.color)
